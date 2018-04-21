d341f6c9c73dc5f46010e53e980b12ba564983fd
yinyifu <yifuyin@buffalo.edu>
2018-04-21 15:36:38 -0400

    New Logic wow

--- src/app/game/main/main.component.ts
+++ src/app/game/main/main.component.ts
@@ -10,69 +10,55 @@ declare var p5: any;
 export class MainComponent implements OnInit {
 
   socket: SocketIOClient.Socket;
-  myP: any;
-  last_drew ={ isLine:false , x:0 , y:0 };
-  //last_emited = 0;
+  canvas: any;
+  last_drew : point;
 
   constructor() { 
-    this.socket = io.connect('localhost.com:3001');
+    this.socket = io.connect('localhost:3001');
+  }
+
+  onDraw(data) {
+      console.log("heard");
+      this.document.line(data.p1.x, data.p1.y, data.p2.x, data.p2.y);
+      this.document.strokeWeight(4);
   }
 
   ngOnInit() {
-    const s = (p) => {
+    const s = (document) => {
 
-      let canvas;
-      this.myP = p;
-      p.preload = () => {
+      this.document = document;
+      document.preload = () => {
         console.log('preload');
       }
 
-      p.setup = () => {
-        canvas = p.createCanvas(1400, 900);
-        p.background(70,70,70);
+      document.setup = () => {
+        document.createCanvas(1400, 900);
+        document.background(70,70,70);
       }
 
-      
-      
-      p.draw = () => {
-        if(p.mouseIsPressed){
-          //var c = p.color(50, 50, 50); // Define color 'c'
-          //p.fill(c); // Use color variable 'c' as fill color
-          //p.ellipse(p.mouseX, p.mouseY, 1, 1);
-          //if(this.last_drew.x!=-10 && this.last_drew.y !=-10){
-            //var c = p.color(180, 180, 150); // Define color 'c'
-           // p.fill(c); 
-           if(!this.last_drew.isLine){
-            this.socket.emit('draw', {
-              x1:p.mouseX, y1:p.mouseY, x2:p.mouseX, y2:p.mouseY
-              }
-            );
-            this.last_drew.isLine = true;
-           }else{
-            if(p.mouseX == this.last_drew.x && p.mouseY ==this.last_drew.y){return;}
-            this.socket.emit('draw', {
-              x1:p.mouseX, y1:p.mouseY, x2:this.last_drew.x ,y2:this.last_drew.y
-              }
-            );
-           }
-          this.last_drew.x=p.mouseX;
-          this.last_drew.y = p.mouseY;
+      document.draw = () => {
+        if(document.mouseIsPressed){
+          let event : drawEvent = { p1: { x :document.mouseX, y:document.mouseY }, p2:this.last_drew}
+          this.socket.emit('draw', event);
+          this.onDraw(event);
         }
-        else{
-          this.last_drew.isLine = false;
-        }
-        //this.last_drew = {x:-10,y:-10};
+
+        this.last_drew= { x : document.mouseX, y : document.mouseY};
       }
     }
+
     let player = new p5(s);
-
-
-    this.socket.on('draw',(data) => {
-      this.myP.line(data.x1, data.y1, data.x2, data.y2);
-      this.myP.strokeWeight(4);
-    }
-
-    );
+    this.socket.on('draw', this.onDraw);
   }  //close on ngOnInit
+  
+  interface drawEvent(){
+    p1 : point;
+    p2 : point;
+  }
+
+  interface point(){
+    x : number;
+    y : number;
+  }
 }
 
